48b46bd2258e9a5f1d46467b2e17ff307dc494f7
jimra <mail@janreitsma.de>
2024-01-10 11:10:29 +0100

    updated loadtest.sh

--- custom-exporter-loadtest/loadtest.sh
+++ custom-exporter-loadtest/loadtest.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
-number_of_exporters=(10 20 40 80 100 200 400)
-number_of_metrics=(100 200 400 800)
+number_of_exporters=(10 50 100 200 300)
+number_of_metrics=(100 200 300)
+file=query_list
+timestamp=$(date +%+y%+m%+d%+k%+M)
+logfile=log/loadtest_log_$timestamp.txt
+datafile=log/loadtest_data_$timestamp.csv
+echo "time;num_exporter;num_metrics;query;jq_selector;exporter;description;value" > $datafile
 
-
-function zpquery() {
-   if [[ -z "$1" ]]; then echo "usage: zpquery <PromQL Query> [<jq selector>]";
-   else curl -s "http://10.0.0.116:9090/api/v1/query" --data-urlencode "query=$1" | jq $2
-   fi
+function kills() {
+    echo "Looking for scripts to kill ..."
+    list1=$(ps -ax | grep -E "python3 ./main.py" | grep -v -e "grep" | cut -d ' ' -f 1);
+    if [[ -n $list1 ]]; then
+        echo "found pids to kill: $list1"
+        for pid in $list1; do kill $pid; done
+    else echo "nothing found."
+    fi
 }
 
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" > auto_test_log.txt
-echo "$(date): ---Starting Load Testing Script---" >> auto_test_log.txt
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >> auto_test_log.txt
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" | tee -a $logfile
+echo "$(date): ---Starting Load Testing Script---" | tee -a $logfile
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" | tee -a $logfile
 
-echo "Looking for scripts to kill ..."
-list1=$(ps -ax | grep -E "python3 ./main.py" | grep -v -e "grep" | cut -d ' ' -f 1);
-if [[ -n $list1 ]]; then
-    echo "found pids to kill: $list1"
-    for pid in $list1; do kill $pid; done
-else echo "nothing found."
-fi
+kills
 
-for i in "${number_of_exporters[@]}"; do
-    for j in "${number_of_metrics[@]}"; do
-    echo "$(date): $i exporters, $j metrics each"
-    echo "$(date): $i exporters, $j metrics each" >> auto_test_log.txt
-    ./main.py $i $j &
+for expcount in "${number_of_exporters[@]}"; do
+    for metcount in "${number_of_metrics[@]}"; do
+    echo "$(date): $expcount exporters, $metcount metrics each"
+    ./main.py $expcount $metcount &
 
-    pause=90   # pause sollte mind 1 Minute sein, damit HTTP SD abgefragt wird und Endpunkte bekannt sind.
-    echo "---Waiting $pause secs for prometheus server to scrape HTTP service discovery---"
+    pause=90   # pause sollte mind 1 Minute (65) sein, damit HTTP SD abgefragt wird und Endpunkte bekannt sind.
+    echo "---Waiting $pause secs for prometheus server to scrape HTTP service discovery---" | tee -a $logfile
 
     # pause countdown
     while [ $pause -gt 0 ]; do
@@ -38,25 +39,29 @@ for i in "${number_of_exporters[@]}"; do
        sleep 1
        done
 
-    query_1=$(curl -s 10.0.0.116:9090/metrics |  grep -E "prometheus_sd_discovered_targets{config=\"load_test\"")
-    # start Änderungen
-    query_2=$(curl -s 10.0.0.116:9090/metrics |  grep -E "prometheus_target_sync_length_seconds{scrape_job=\"load_test\",quantile=\"0.5\"}")
-    query_3=$(curl -s 10.0.0.116:9090/metrics |  grep -E "prometheus_target_sync_length_seconds_sum{scrape_job=\"load_test\"}")
+    # process query list query_list
+    while read -r line; do
+    if [[ ! $line =~ ^# && -n $line ]] ; then
+        # split line in query and jq argument:
+        query=$(echo "$line" | cut -d ";" -f 1)
+        jqselector=$(echo "$line" | cut -d ";" -f 2)
+        exporter=$(echo "$line" | cut -d ";" -f 3)
+        description=$(echo "$line" | cut -d ";" -f 4)
+        echo "---------------- new query: --------------------"
+        echo "Querying: $query"
+        echo  "JQ selector: $jqselector"
 
-    # hier weitere queries einfügen  und loggen!
-    echo "$(date): $query_1"
-    echo "$(date): $query_1" >> auto_test_log.txt
-    echo "$(date): $query_2"
-    echo "$(date): $query_2" >> auto_test_log.txt
-    echo "$(date): $query_3"
-    echo "$(date): $query_3" >> auto_test_log.txt
-    # stop Änderungen
+        value=$(curl -s http://10.0.0.116:9090/api/v1/query --data-urlencode "query=$query" | jq "$jqselector")
+        echo "Value: $value"
+        echo "$(date +%s);$expcount;$metcount;$query;$jqselector;$exporter;$description;$value" | tee -a $datafile
 
-    #echo "$(date): $query_1" >> auto_test_log.txt
-    sleep 1
-    script_pid=$(ps -ax | grep -E "python3 ./main.py $i $j" | grep -v -e "grep" | cut -d ' ' -f 1)
-    echo $script_pid
-    echo "killing pid $script_pid...";
+    fi
+    done <$file
+
+    script_pid=$(ps -ax | grep -E "python3 ./main.py $expcount $metcount" | grep -v -e "grep" | cut -d ' ' -f 1)
+    echo "killing pid $script_pid..." | tee -a $logfile;
         kill $script_pid;
     done
 done
+
+echo "finished!"
